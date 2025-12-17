d129243ca162aabf54ae207d671b8a1a324988e5
sejal9588 <93347436+psejal@users.noreply.github.com>
2025-12-17 10:55:53 -0500

    email

--- server.js
+++ server.js
@@ -24,14 +24,7 @@ mongoose.connect(process.env.MONGODB_URI)
 });
 console.log("ADMIN_EMAIL:", process.env.ADMIN_EMAIL);
 console.log("ADMIN_EMAIL_PASS:", process.env.ADMIN_PASS ? "Loaded ✅" : "❌ Missing");
-//mail transporter
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.ADMIN_EMAIL,
-    pass: process.env.ADMIN_PASS,  // 16-digit Gmail App Password
-  }
-});
+
 
 
 /*--------Models-----------*/
